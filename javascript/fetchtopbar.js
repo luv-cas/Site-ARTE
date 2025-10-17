7eb6d3e48b103717e1f98a3e95d2978bf97124be
Lucas <lucascarneirojacomelli@gmail.com>
2025-10-17 12:02:46 -0300

    topbar pronta, botao fala criado

---
+++ javascript/fetchtopbar.js
@@ -0,0 +1,6 @@
+ fetch("./topbar.html")
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("topbar").innerHTML = data;
+        })
+        .catch(err => console.error("Erro ao carregar a topbar:", err));
