58fff4a2aa1e586c677ae6a400127d25f8383c3f
Lucas Carneiro <208729620+luv-cas@users.noreply.github.com>
2025-11-05 07:36:02 -0300

    Update botaoTTS.js

--- javascript/botaoTTS.js
+++ javascript/botaoTTS.js
@@ -1,28 +1,27 @@
 fazerFala = function(id){
-    if (speechSynthesis.speaking) {
-        speechSynthesis.cancel();
-    }
-                let utterance = new SpeechSynthesisUtterance
-                utterance.lang = "pt-BR"
-                utterance.volume
-                utterance.text = document.getElementById(id).innerHTML
-                console.log(document.getElementById(id).innerHTML)
-                speechSynthesis.speak(utterance)
+  if (speechSynthesis.speaking) {
+    speechSynthesis.cancel();
+  }
+  let utterance = new SpeechSynthesisUtterance
+  utterance.lang = "pt-BR"
+  utterance.volume = 1;
+  utterance.text = document.getElementById(id).innerHTML
+  console.log(document.getElementById(id).innerHTML)
+  speechSynthesis.speak(utterance)
 }
     // -- Parte da galeria -- //
 function falarModal() {
   if (speechSynthesis.speaking) {
-    speechSynthesis.cancel();
+    speechSynthesis.cancel()
   }
 
-  const titulo = document.getElementById("modalTitulo").textContent;
-  const descricao = document.getElementById("modalDescricao").textContent;
+  const titulo = document.getElementById("modalTitulo").textContent
+  const descricao = document.getElementById("modalDescricao").textContent
 
-  const texto = `${titulo}. ${descricao}`;
-  const utterance = new SpeechSynthesisUtterance(texto);
-  utterance.lang = "pt-BR";
-  utterance.rate = 1;
-  utterance.volume = 1;
-
-  speechSynthesis.speak(utterance);
+  const texto = `${titulo}. ${descricao}`
+  const utterance = new SpeechSynthesisUtterance(texto)
+  utterance.lang = "pt-BR"
+  utterance.rate = 1
+  utterance.volume = 1
+  speechSynthesis.speak(utterance)
 }
