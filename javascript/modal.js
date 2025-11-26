da4f520a237e5eb22de81663c98e3144a6580739
Lucas <lucascarneirojacomelli@gmail.com>
2025-11-25 21:17:51 -0300

    Revert "commit final? (amem)"
    This reverts commit e1cdb9a2e28e51dd0c442a7f9b71e71b04cfd3ee.

---
+++ javascript/modal.js
@@ -0,0 +1,24 @@
+ // ===== LÓGICA DO MODAL =====
+    const modal = document.getElementById('modal');
+    const modalTitulo = document.getElementById('modalTitulo');
+    const modalDescricao = document.getElementById('modalDescricao');
+    const modalImagem = document.getElementById('modalImagem')
+
+    function abrirModal(titulo, descricao, imagem) {
+      modalTitulo.textContent = titulo;
+      modalDescricao.textContent = descricao;
+      if (imagem) modalImagem.src = imagem;
+      modal.style.display = 'flex';
+    }
+
+    function fecharModal() {
+      modal.style.display = 'none';
+      
+      if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    }
+  }
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) fecharModal();
+    })
