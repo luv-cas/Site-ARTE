e1cdb9a2e28e51dd0c442a7f9b71e71b04cfd3ee
Lucas <lucascarneirojacomelli@gmail.com>
2025-11-25 21:16:45 -0300

    commit final? (amem)

--- javascript/fetchtopbar.js
+++
@@ -1,6 +0,0 @@
- fetch("topbar.html")
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("topbar").innerHTML = data;
-        })
-        .catch(err => console.error("Erro ao carregar a topbar:", err));
